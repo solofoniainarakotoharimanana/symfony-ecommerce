126336cc4699c450904c8b4ecbfdae99b0431eb9
solofoniaina <solofoniainarakotoharimanana@gmail.com>
2025-05-21 11:45:43 +0300

    Feat: Show products to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', methods:['GET'])]
+    public function index(ProductsRepository $productsRepo): Response
     {
-        return $this->render('home/index.html.twig');
+
+        return $this->render('home/index.html.twig', [
+            'products' => $productsRepo->findBy([], ['id' => 'DESC'])
+        ]);
+    }
+
+    #[Route('/product/{id}', name:'app_show_product', methods: ['GET'])]
+    public function showProduct(Products $product, ProductsRepository $productsRepo):Response
+    {
+        $latestProduct = $productsRepo->findBy([], ['id' => 'DESC'], 5);
+
+        return $this->render('home/show.html.twig', [
+            'product' => $product,
+            'latestProducts' => $latestProduct
+        ]);
+
     }
 }
